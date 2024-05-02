244473a4a20277eba00e6d88a17292dd9e0834de
Chrissie <23245656@shrewsbury.ac.uk>
2024-05-02 11:42:51 +0100

    Added list formattinf and other stuff

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let filenamesIframe = document.getElementById("doc_names");
 let navbar = document.getElementById("navbar");
 let body = document.getElementById("bodytext");
@@ -30,9 +31,9 @@ filenamesIframe.onload = function () {
             navbarItemDiv.appendChild(navbarItemLabel);
             navbar.appendChild(navbarItemDiv);
 
-            
+
         }
-        
+
 
     }
 }
@@ -47,20 +48,8 @@ function displayFile(filename) {
 
     individualFileIframe.onload = function () {
         let currentFileContentRaw = individualFileIframe.contentWindow.document.body.innerText;
-
-        
-
         body.innerHTML = markdownParser(currentFileContentRaw);
         console.log("success");
-
-
-
-
-
-        
-
-        
-
     }
 }
 
@@ -71,20 +60,63 @@ function markdownParser(text) {
     let output = text
     // this handles most things
 
-        .replace(/^###### (.*$)/gm, "<h6>$1</h6>")
-        .replace(/^##### (.*$)/gm, "<h5>$1</h5>")
-        .replace(/^#### (.*$)/gm, "<h4>$1</h4>")
-        .replace(/^### (.*$)/gm, "<h3>$1</h3>")
-        .replace(/^## (.*$)/gm, "<h2>$1</h2>")
-        .replace(/^# (.*$)/gm, "<h1>$1</h1>")
-        .replace(/(\[img\])\((.*)\)/gm, '<img src="$2">')
-        .replace(/\[(.*)\]\((.*)\)/gm, '<a href="$2">$1</a>')
-        .replace(/(\[jump )(.*)(\])/gm, '<p class="hidden" id="$2">')
-        .replace(/\*\*(.*)\*\*/gm, '<strong>$1</strong>')
-        .replace(/\*(.*)\*/gm, '<em>$1</em>');
+        .replace(/^###### (.*$)/gm, '<h6 placeholder="placeholder">$1</h6>')
+        .replace(/^##### (.*$)/gm, '<h5 placeholder="placeholder">$1</h5>')
+        .replace(/^#### (.*$)/gm, '<h4 placeholder="placeholder">$1</h4>')
+        .replace(/^### (.*$)/gm, '<h3 placeholder="placeholder">$1</h3>')
+        .replace(/^## (.*$)/gm, '<h2 placeholder="placeholder">$1</h2>')
+        .replace(/^# (.*$)/gm, '<h1 placeholder="placeholder">$1</h1>')
+        .replace(/(\[img\])\((.*?)\)/gm, '<img src="$2">')
+        .replace(/\[(.*?)\]\((.*?)\)/gm, '<a href="$2">$1</a>')
+        .replace(/\*\*(.*?)\*\*/gm, '<strong>$1</strong>')
+        .replace(/\*(.*?)\*/gm, '<em>$1</em>');
 
     let outArr = output.split("\n");
+    let isList = false;
+    let isOrdered = false;
+
     for (var k = 0; k < outArr.length; k++) {
+        if (outArr[k].substr(0, 5) == "[jump") {
+            outArr[k + 1] = outArr[k + 1].replace('placeholder="placeholder"', 'id="' + outArr[k].slice(6, -1) + '"');
+            outArr[k] = "";
+        }
+    }
+
+    for (var k = 0; k < outArr.length; k++) {
+
+        if (outArr[k].substr(0, 9) == "[ordered]") {
+            outArr[k] = "<ol>";
+            isOrdered = true;
+            isList = true;
+            continue;
+        }
+
+        if (outArr[k].substr(0, 11) == "[unordered]") {
+            outArr[k] = "<ul>";
+            isOrdered = false;
+            isList = true;
+            continue;
+        }
+
+        if (isList && outArr[k][0] == "*") {
+            outArr[k] = "<li>" + outArr[k].slice(2) + "</li>"
+            continue;
+        }
+
+        if (outArr[k].substr(0, 5) == "[end]") {
+            if (isOrdered) {
+                outArr[k] = "</ol>";
+                continue;
+            } else {
+                outArr[k] = "</ul>";
+                continue;
+            }
+        }
+
+    }
+
+    for (var k = 0; k < outArr.length; k++) {
+
         if (outArr[k][0] == "<") {
             continue;
         } else {
@@ -98,6 +130,7 @@ function markdownParser(text) {
     for (var l = 0; l < outArr.length; l++) {
         output = output + outArr[l] + "\n";
     }
-    
+
+    console.log(output);
     return output;
 }
